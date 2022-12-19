82676deee4e9ca8be9498cd19cf0678495dcb968
Brian Pak Chan <brian@brians-air.lan>
2022-12-19 14:40:05 +0800

    third update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://noahflk.com/',
+  site: 'https://pach622.github.io',
   integrations: [
     sitemap(),
     tailwind({
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const AUTHOR = 'Brian';
 export const HANDLE_TWITTER = 'brian_pk_chan';
-export const HANDLE_GITHUB = 'noahflk';
+export const HANDLE_GITHUB = 'pach622';
 export const EMAIL = 'brianchan622@gmail.com';
